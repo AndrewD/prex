f32c98873a0095ac7fb8d75c0bdcae04faf60c98
Andrew Dennison <andrewd.lists@gmail.com>
2009-07-04 13:55:01 +1000

    stdarg fix for gcc >= 4
    * __builtin_va_start in gcc from 3.1
    * __builtin_stdarg_start removed in gcc 4.4

--- include/i386/stdarg.h
+++ include/i386/stdarg.h
@@ -45,7 +45,11 @@ typedef _BSD_VA_LIST_	va_list;
 #endif
 
 #ifdef __GNUC__
+#if (__GNUC__ >= 4)
+#define	va_start(ap, last)	__builtin_va_start((ap), (last))
+#else
 #define	va_start(ap, last)	__builtin_stdarg_start((ap), (last))
+#endif
 #define	va_arg			__builtin_va_arg
 #define	va_end			__builtin_va_end
 #define	va_copy(dest, src)	__builtin_va_copy((dest), (src))
